af3389fd66e49155003972680b75ae096bbdeb86
lucastorterotot <lucas.torterotot@gmail.com>
2019-01-28 13:29:33 +0100

    Changed inputs for 2017

--- prod/Config_RunonMiniaod.py
+++ prod/Config_RunonMiniaod.py
@@ -212,15 +212,18 @@ process.out.outputCommands.append("keep *_slimmedGenJetsAK8_*_*")
 if runOnData:
         if runOnLegacy: 
            process.source = cms.Source("PoolSource",
-           fileNames = cms.untracked.vstring("/store/data/Run2017D/SinglePhoton/MINIAOD/17Nov2017-v1/710000/48C05C67-16D9-E711-BC16-4C79BA320401.root")
+           fileNames = cms.untracked.vstring("/store/data/Run2017F/SinglePhoton/MINIAOD/17Nov2017-v1/50000/004425E2-EBDE-E711-8F80-008CFAF5543A.root"),
+                                       inputCommands =  cms.untracked.vstring("keep *","drop  *_isolatedTracks__*")
            )
         else:
              process.source = cms.Source("PoolSource",
-             fileNames = cms.untracked.vstring("/store/data/Run2017D/SinglePhoton/MINIAOD/17Nov2017-v1/710000/48C05C67-16D9-E711-BC16-4C79BA320401.root")
+             fileNames = cms.untracked.vstring("/store/data/Run2017F/SinglePhoton/MINIAOD/17Nov2017-v1/50000/004425E2-EBDE-E711-8F80-008CFAF5543A.root"),
+                                       inputCommands =  cms.untracked.vstring("keep *","drop  *_isolatedTracks__*")
              )
 else:
        process.source = cms.Source("PoolSource",
-           fileNames = cms.untracked.vstring("/store/mc/RunIIFall17MiniAOD/GJet_Pt-15To6000_TuneCP5-Flat_13TeV_pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/40000/2C41D5C8-5DDA-E711-876C-1866DA87C2CD.root")
+           fileNames = cms.untracked.vstring("/store/mc/RunIIFall17MiniAOD/GJet_Pt-15To6000_TuneCP5-Flat_13TeV_pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/40000/2C41D5C8-5DDA-E711-876C-1866DA87C2CD.root"),
+                                       inputCommands =  cms.untracked.vstring("keep *","drop  *_isolatedTracks__*")
            )
 
 process.load("Configuration.Geometry.GeometryRecoDB_cff")
