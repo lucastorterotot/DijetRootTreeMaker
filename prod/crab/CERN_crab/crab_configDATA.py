36a674aef45cea5cd9c10c7963ba5198752c158e
lucastorterotot <lucas.torterotot@gmail.com>
2019-01-29 14:22:23 +0100

    CRAB setup for 2017 ready.

--- prod/crab/CERN_crab/crab_configDATA.py
+++ prod/crab/CERN_crab/crab_configDATA.py
@@ -2,16 +2,16 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = config()
 
-config.General.requestName = 'TMP-TEST'
+config.General.requestName = 'TMP-TEST-2017-data'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '/afs/cern.ch/work/l/ltortero/JEC-task/CMSSW_10_2_5/src/CMSDIJET/DijetRootTreeMaker/prod/Config_RunonMiniaod.py'
+config.JobType.psetName = '/afs/cern.ch/work/l/ltortero/JEC-task/CMSSW_9_4_10/src/CMSDIJET/DijetRootTreeMaker/prod/Config_RunonMiniaod.py'
 config.JobType.pyCfgParams = ['isRunonData=True']
 
-config.Data.inputDataset = '/SinglePhoton/Run2016B-03Feb2017_ver2-v2/MINIAOD'
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+config.Data.inputDataset = '/SinglePhoton/Run2017B-17Nov2017-v1/MINIAOD'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 250
 config.Data.publication = False
--- prod/crab/CERN_crab/crab_configMC.py
+++ prod/crab/CERN_crab/crab_configMC.py
@@ -1,19 +1,19 @@
-from CRABClient.UserUtilities import config
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = config()
 
-config.General.requestName = 'GJet-RunIISpring16MiniAODv1_v2_endcapPhoton_20M'
+config.General.requestName = 'TMP-TEST-2017-mc'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '/afs/cern.ch/work/h/hlattaud/private/production_GJet/NewEGcorr/test_release/CMSSW_9_4_10/src/CMSDIJET/DijetRootTreeMaker/prod/Config_RunonMiniaod.py'
+config.JobType.psetName = '/afs/cern.ch/work/l/ltortero/JEC-task/CMSSW_9_4_10/src/CMSDIJET/DijetRootTreeMaker/prod/Config_RunonMiniaod.py'
 
-config.Data.inputDataset = '/GJet_Pt-15To6000_TuneCUETP8M1-Flat_13TeV_pythia8_20M/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+config.Data.inputDataset = '/GJet_Pt-15To6000_TuneCP5-Flat_13TeV_pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1/MINIAODSIM'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 500000
 config.Data.publication = False
-config.Data.outLFNDirBase = '/store/group/phys_jetmet/hlattaud/GammaJet/'
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
-config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T3_FR_IPNL'
 
