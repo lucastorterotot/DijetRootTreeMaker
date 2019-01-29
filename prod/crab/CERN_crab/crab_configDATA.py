6e817a464fc031805a1463ccef1781fe07c26e94
lucastorterotot <lucas.torterotot@gmail.com>
2019-01-29 14:36:23 +0100

    CRAB setup for 2018 ready.

--- prod/crab/CERN_crab/crab_configDATA.py
+++ prod/crab/CERN_crab/crab_configDATA.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = config()
 
-config.General.requestName = 'TMP-TEST'
+config.General.requestName = 'TMP-TEST-2018-data'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
@@ -10,8 +10,8 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/afs/cern.ch/work/l/ltortero/JEC-task/CMSSW_10_2_5/src/CMSDIJET/DijetRootTreeMaker/prod/Config_RunonMiniaod.py'
 config.JobType.pyCfgParams = ['isRunonData=True']
 
-config.Data.inputDataset = '/SinglePhoton/Run2016B-03Feb2017_ver2-v2/MINIAOD'
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+config.Data.inputDataset = '/EGamma/Run2018B-26Sep2018-v1/MINIAOD'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/Era/Prompt/Cert_316998-319312_13TeV_PromptReco_Collisions18_JSON_eraB.txt' # TODO : update if available
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 250
 config.Data.publication = False
