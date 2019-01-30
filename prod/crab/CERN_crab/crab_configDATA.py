a2c52ce2c2554af20671cd73feeab9645fc38549
lucastorterotot <lucas.torterotot@gmail.com>
2019-01-30 11:21:29 +0100

    updated config.Data.lumiMask in crab_configDATA.py

--- prod/crab/CERN_crab/crab_configDATA.py
+++ prod/crab/CERN_crab/crab_configDATA.py
@@ -11,7 +11,7 @@ config.JobType.psetName = '/afs/cern.ch/work/l/ltortero/JEC-task/CMSSW_10_2_5/sr
 config.JobType.pyCfgParams = ['isRunonData=True']
 
 config.Data.inputDataset = '/EGamma/Run2018B-26Sep2018-v1/MINIAOD'
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/Era/Prompt/Cert_316998-319312_13TeV_PromptReco_Collisions18_JSON_eraB.txt' # TODO : update if available
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 250
 config.Data.publication = False
