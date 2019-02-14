18f9c556f738932dc0267d4106be44bda680558a
lucastorterotot <lucas.torterotot@gmail.com>
2019-02-14 08:50:46 +0100

    Updated Config_RunonMiniaod.py to get a better output name.

--- prod/Config_RunonMiniaod.py
+++ prod/Config_RunonMiniaod.py
@@ -136,12 +136,12 @@ process.GlobalTag = GlobalTag(process.GlobalTag, options.globalTag,'')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxEvents))
 
 
-outputsuffix = '_MC'
+outputsuffix = 'MC'
 if runOnData:
-   outputsuffix = '_DATA'
+   outputsuffix = 'DATA'
 
 process.TFileService=cms.Service("TFileService",
-                                 fileName=cms.string('2018_'+outputsuffix+'.root'),
+                                 fileName=cms.string('DijetRootTreeMaker_2018_'+outputsuffix+'.root'),
                                  #fileName=cms.string(THISROOTFILE),
                                  closeFileFast = cms.untracked.bool(True)
                                  )
