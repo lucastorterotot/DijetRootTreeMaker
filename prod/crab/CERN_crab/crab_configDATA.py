7561d5b04a0bc6f855f20db74035d6d9041a8040
lucastorterotot <lucas.torterotot@gmail.com>
2019-02-15 12:51:31 +0100

    Updated crab configs for 2018.

--- prod/crab/CERN_crab/crab_configDATA.py
+++ prod/crab/CERN_crab/crab_configDATA.py
@@ -2,18 +2,19 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = config()
 
-config.General.requestName = 'TMP-TEST-2018-data'
+config.General.requestName = '2018-DATA'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/afs/cern.ch/work/l/ltortero/JEC-task/CMSSW_10_2_5/src/CMSDIJET/DijetRootTreeMaker/prod/Config_RunonMiniaod.py'
 config.JobType.pyCfgParams = ['isRunonData=True']
+config.JobType.priority = 5
 
 config.Data.inputDataset = '/EGamma/Run2018B-26Sep2018-v1/MINIAOD'
 config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
-config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 250
+config.Data.splitting = 'Automatic' #  'LumiBased'
+#config.Data.unitsPerJob = 250
 config.Data.publication = False
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 
@@ -21,5 +22,5 @@ config.section_("Debug")
 config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
 
 config.Site.storageSite = 'T3_FR_IPNL'
-config.Site.whitelist = ['T2_CH_CERN', 'T3_FR_IPNL']
+#config.Site.whitelist = ['T2_CH_CERN', 'T3_FR_IPNL']
 
