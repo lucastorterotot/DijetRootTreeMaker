a65ae1c44d4ac818ca23e90aefa19affc3d6f204
lucastorterotot <lucas.torterotot@gmail.com>
2020-02-21 10:27:07 +0100

    Update samples for MC

--- prod/crab/CERN_crab/crab_configMC.py
+++ prod/crab/CERN_crab/crab_configMC.py
@@ -9,7 +9,7 @@ config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/afs/cern.ch/work/l/ltortero/JEC-task/CMSSW_9_4_10/src/CMSDIJET/DijetRootTreeMaker/prod/Config_RunonMiniaod.py'
 
-config.Data.inputDataset = '/GJet_Pt-15To6000_TuneCP5-Flat_13TeV_pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1/MINIAODSIM'
+config.Data.inputDataset = '/GJets_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v1/MINIAODSIM'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 500000
 config.Data.publication = False
